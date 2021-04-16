244729ecfcb5058d9e1bef53b8b0597defd3dd53
yangwonpark <ailey@nate.com>
2021-04-16 17:56:18 +0900

    8. tuple

---
+++ tuple.ts
@@ -0,0 +1,18 @@
+let x: [string, number];
+
+x = ["hello", 39];
+
+x = [10, "Mark"];
+
+// x[3] = "world";
+
+const person: [string, number] = ["Mark", 39];
+
+// 분해 할당
+const [first, second] = person;
+
+
+
+
+
+
